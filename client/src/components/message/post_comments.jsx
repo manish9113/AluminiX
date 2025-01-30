13a601f1d3a32d47673d478396199aa1eea1352b
manish9113 <133559216+manish9113@users.noreply.github.com>
2025-01-30 07:55:43 +0530

    Update post_comments.jsx backend url

--- client/src/components/message/post_comments.jsx
+++ client/src/components/message/post_comments.jsx
@@ -13,7 +13,7 @@ function PostComments() {
    useEffect(() => {
      const fetchPosts = async () => {
        try {
-         const response = await axios.get("http://localhost:5000/allPosts");
+         const response = await axios.get("https://aluminix.onrender.com/allPosts");
          console.log(response.data.posts[0].postProfileUrl);
          setRes(response.data.posts);
         
